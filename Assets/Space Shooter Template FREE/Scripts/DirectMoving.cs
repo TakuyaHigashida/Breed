3b49bae15b33ff01fe2de3f5143d76d87c79b048
DEV1-10\GA2A <h1202019037@hiratagakuen.onmicrosoft.com>
2021-11-04 12:31:19 +0900

    アセット追加

---
+++ Assets/Space Shooter Template FREE/Scripts/DirectMoving.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script moves the attached object along the Y-axis with the defined speed
+/// </summary>
+public class DirectMoving : MonoBehaviour {
+
+    [Tooltip("Moving speed on Y axis in local space")]
+    public float speed;
+
+    //moving the object with the defined speed
+    private void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        if (transform.position.y > 5 || transform.position.y < -5 || transform.position.x > 9 || transform.position.x < -9)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
